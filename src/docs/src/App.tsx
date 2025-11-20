a1eecc426cf84503fca9ad1d5985b61366b8a7d0
Peter Adelodun <adelodunpeter69@gmail.com>
2025-11-20 12:05:28 +0100

    Create DocsPage component and update App.tsx

--- src/docs/src/App.tsx
+++ src/docs/src/App.tsx
@@ -1,38 +1,12 @@
-import { useState } from 'react';
 import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import DocContent from './components/DocContent';
-import {
-  gettingStartedData,
-  connectionsData,
-  queryEditorData,
-  schemaExplorerData,
-  dataExplorerData,
-  backupRestoreData,
-  settingsData,
-} from './data';
+import DocsPage from './pages/DocsPage';
 import './App.css';
 
-const docMap: Record<string, any> = {
-  'getting-started': gettingStartedData,
-  'connections': connectionsData,
-  'query-editor': queryEditorData,
-  'schema-explorer': schemaExplorerData,
-  'data-explorer': dataExplorerData,
-  'backup-restore': backupRestoreData,
-  'settings': settingsData,
-};
-
 function App() {
-  const [activeSection, setActiveSection] = useState('getting-started');
-
   return (
     <div className="app">
       <Header />
-      <div className="main-container">
-        <Sidebar activeSection={activeSection} onSectionChange={setActiveSection} />
-        <DocContent data={docMap[activeSection]} />
-      </div>
+      <DocsPage />
     </div>
   );
 }
---
+++ src/docs/src/pages/DocsPage.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import DocContent from '../components/DocContent';
+import {
+  gettingStartedData,
+  connectionsData,
+  queryEditorData,
+  schemaExplorerData,
+  dataExplorerData,
+  backupRestoreData,
+  settingsData,
+} from '../data';
+
+const docMap: Record<string, any> = {
+  'getting-started': gettingStartedData,
+  'connections': connectionsData,
+  'query-editor': queryEditorData,
+  'schema-explorer': schemaExplorerData,
+  'data-explorer': dataExplorerData,
+  'backup-restore': backupRestoreData,
+  'settings': settingsData,
+};
+
+export default function DocsPage() {
+  const [activeSection, setActiveSection] = useState('getting-started');
+
+  return (
+    <div className="main-container">
+      <Sidebar activeSection={activeSection} onSectionChange={setActiveSection} />
+      <DocContent data={docMap[activeSection]} />
+    </div>
+  );
+}
