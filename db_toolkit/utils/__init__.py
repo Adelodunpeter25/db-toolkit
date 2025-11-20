8a1f52aa706dfdde81dd237d7165334c0afa9dd4
Peter Adelodun <adelodunpeter69@gmail.com>
2025-11-20 22:00:08 +0100

    Add beautiful PDF utility module with ReportLab for report generation

---
+++ db_toolkit/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utility modules."""
+
+from db_toolkit.utils.pdf import PDFReport, format_bytes, format_duration, truncate_text
+
+__all__ = ["PDFReport", "format_bytes", "format_duration", "truncate_text"]
